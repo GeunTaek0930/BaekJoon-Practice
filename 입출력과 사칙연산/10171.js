5d7c7b2d24a8cce08651eeb9cadda93c1864fd6c
이근택 <101080311+GeunTaek0930@users.noreply.github.com>
2022-03-14 15:12:07 +0900

    20220314

---
+++ 입출력과 사칙연산/10171.js
@@ -0,0 +1,10 @@
+console.log(`1번풀이`);
+
+console.log(`\\    /\\`);
+console.log(` )  ( ')` );
+console.log(`(  /  )`);
+console.log(` \\(__)|`);
+
+console.log(`2번풀이`);
+
+console.log('\\    /\\\n )  ( \')\n(  /  )\n \\(__)|');
