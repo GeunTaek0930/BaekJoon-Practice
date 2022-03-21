e0f057777c08bca9c455d0df57bc72c0bb3bcc30
이근택 <101080311+GeunTaek0930@users.noreply.github.com>
2022-03-21 10:36:41 +0900

    2022-03-21 별찍기1 수정

--- for문/2438.js
+++ for문/2438.js
@@ -6,3 +6,11 @@ for (let i = 1; i <=input; i++) {
   star= star+ "*"
   console.log(star);
 }
+
+/* repeat사용
+let input = require('fs').readFileSync('/dev/stdin').toString();
+
+for (let i = 1; i <=input; i++) {
+  console.log('*'.repeat(i));
+}
+*/
